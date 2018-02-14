bb7bf070db7bfd4335db61740536ae00ca0a4db3
Vroumgx <35525721+Vroumgx@users.noreply.github.com>
2018-02-14 16:57:05 +0100

    Correctif problème d'encodage Windows

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,13 @@
 <?php
-	$server 	= 'localhost';
+	$server     = 'localhost';
 	$username   = '';
 	$password   = '';
 	$database   = '';
 
 	try{
-		$conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+		$conn = new PDO("mysql:host=$server;dbname=$database", $username, $password,
+				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
 	}
 	catch(PDOException $e){
 	    echo "Erreur de connexion : " . $e->getMessage();
